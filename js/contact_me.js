ddebbc60ba1430a0519ee119d9da1241610c2c4d
ajithmarshall <ajit.marshal92@gmail.com>
2016-10-16 22:16:50 +0530

    new

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/ajit.marshall92@gmail.com",
+                url: "//formspree.io/mail.dreamstudio@gmail.com",
                 type: "POST",
                 data: {
                     name: name,
